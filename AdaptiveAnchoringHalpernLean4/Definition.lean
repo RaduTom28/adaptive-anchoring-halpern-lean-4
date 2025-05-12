2c4a630876fcf1d8b9a37932fc507117cb91a63a
Radu Tomescu <radut4321@gmail.com>
2025-05-12 22:34:19 +0300

    Update Definition.lean

--- AdaptiveAnchoringHalpernLean4/Definition.lean
+++ AdaptiveAnchoringHalpernLean4/Definition.lean
@@ -3,6 +3,7 @@ import Mathlib.Analysis.InnerProductSpace.Basic
 
 --todo: include x_n
 
+local notation "⟪" x ", " y "⟫" => inner ℝ x y
 
 structure Iteration (E : Type) [NormedAddCommGroup E] [InnerProductSpace ℝ E] [CompleteSpace E] where
   x_0: E
@@ -16,10 +17,13 @@ structure Iteration (E : Type) [NormedAddCommGroup E] [InnerProductSpace ℝ E] 
 variable {H : Type} [NormedAddCommGroup H] [InnerProductSpace ℝ H] [CompleteSpace H]
 
 
-def phi (I : Iteration H) (n : Nat) := sorry
+def phi (I : Iteration H) (n : Nat) := 2 * ⟪x n - T (x n), x₀ - x n⟫ / (‖x n - T (x n)‖ ^ 2)
 
-def x_n (I : Iteration H) (n : Nat) := sorry
+def x_n (I : Iteration H) (n : Nat) : H
+| 0     => x₀
+| (n+1) =>
+    let ϕ := 2 * ⟪x n - T (x n), x₀ - x n⟫ / (‖x n - T (x n)‖ ^ 2)
+    (1 / (ϕ + 1)) • x₀ + (ϕ / (ϕ + 1)) • T (x n)
 
-
-lemma phi_n_geq_n (I : Iteration H) (x y : H) (n : Nat): phi I n ≥ n := by
- sorry
+-- lemma phi_n_geq_n (I : Iteration H) (x y : H) (n : Nat): phi I n ≥ n := by
+--  sorry
