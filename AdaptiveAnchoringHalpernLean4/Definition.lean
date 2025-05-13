f6df8494d443f87bf5871318522e00cfab3224f1
Radu Tomescu <radut4321@gmail.com>
2025-05-13 22:37:53 +0300

    made progress on base bound induction proof

--- AdaptiveAnchoringHalpernLean4/Definition.lean
+++ AdaptiveAnchoringHalpernLean4/Definition.lean
@@ -25,24 +25,40 @@ match n with
 noncomputable def phi (I : Iteration H)(n : Nat) : ℝ := 2 * ⟪x I (n-1) - I.T (x I (n-1)), I.x_0 - x I (n-1)⟫ / (‖x I (n-1) - I.T (x I (n-1))‖ ^ 2) + 1
 
 
-@[simp]
+
 lemma base_case_recurrence (I : Iteration H) : x I 0 = I.x_0 := by
   unfold x
   simp
 
-@[simp]
+
 lemma first_recurrence_term (I : Iteration H) : x I 1 = (2:ℝ)⁻¹ • I.x_0 + (2:ℝ)⁻¹ • I.T I.x_0 := by
   unfold x
   simp
   norm_num
+  rw [base_case_recurrence]
+  norm_num
 
-@[simp]
+lemma first_phi (I : Iteration H) : phi I 1 = 1 := by
+  unfold phi
+  simp
+  rw [base_case_recurrence]
+  simp
+
 lemma recurrence_subst_phi (I : Iteration H) (n : Nat) : x I (n+1) = (1 / (phi I (n+1) + 1)) •  I.x_0 + (phi I (n+1)  / (phi I (n+1)  + 1)) •  I.T (x I n) := by
   simp
   unfold phi
   norm_num
   rw [x]
   simp
+
+omit [CompleteSpace H] in
+lemma split_prod (x : H) : (2:ℝ) • x = x + x := by
+  exact two_smul ℝ x
+
+-- noted
+theorem essential_1 (x y : H) : ‖x + y‖^2 = ‖x‖^2 + ‖y‖^2 + 2 * ⟪x,y⟫ := by
+  rw [@norm_add_sq_real]
+  rw [add_right_comm]
 
 -- @[simp]
 -- lemma aux_simp (f : ℝ) : (f + 1) / f = (f + 1) • f⁻¹ := by
@@ -87,12 +103,6 @@ lemma recurrence_subst_phi (I : Iteration H) (n : Nat) : x I (n+1) = (1 / (phi I
 
 
 
-
-
-
-
-
-
 -- @[simp]
 -- lemma recurrence_rewritten (I : Iteration H) (n : Nat) : I.T (x I n) = x I (n+1) + (1/(phi I (n+1))) • (x I (n+1) - I.x_0) := by
 --   have recurrence := recurrence_subst_phi I n
@@ -101,19 +111,62 @@ lemma recurrence_subst_phi (I : Iteration H) (n : Nat) : x I (n+1) = (1 / (phi I
 --   refine add_eq_of_eq_sub ?_
 
 
-lemma first_bounds (I : Iteration H) (n : ℕ) : (phi I (n+1) ≥ n+1) ∧ (‖(x I (n+1)) - I.T (x I (n+1))‖^2 ≤ 2/(phi I (n+1)) • ⟪ (x I (n+1)) - I.T (x I (n+1)) , I.x_0 - x I (n+1)⟫) := by
+
+-- theorem diff_first_two_terms (I : Iteration H) : ‖ I.T (I.x_0) - I.T (x I 1)‖ ^ 2 = ‖x I 1 - I.T (x I 1)‖^2 + ‖x I 1 - I.x_0‖^2 + 2 * ⟪ x I 1 - I.T (x I 1), x I 1 - I.x_0 ⟫ := by
+-- sorry
+
+omit [InnerProductSpace ℝ H] [CompleteSpace H] in
+theorem comm_operation (x y z : H) : x + x - y - z = (x - y) + (x - z) := by
+  rw [@sub_add_sub_comm]
+  rw [@sub_sub]
+
+theorem square_nonexpansive (I : Iteration H) (x y : H) : ‖I.T x - I.T y‖^2 ≤ ‖x - y‖^2 := by
+  have h1 := I.hTNonExpansive x y
+  mono
+  simp
+
+-- add comparison util
+-- use theorem for minus change inside inner in new have
+
+
+omit [CompleteSpace H] in
+theorem inner_factor_minus (x y0 y1 : H) : ⟪ x, y0 - y1 ⟫ = - ⟪ x , y1 - y0 ⟫ := by
+  rw [inner_sub_right]
+  rw [inner_sub_right]
+  rw [real_inner_comm]
+  simp
+
+lemma first_bounds (I : Iteration H) (n : ℕ) : (phi I (n+1) ≥ n+1) ∧ (‖(x I (n+1)) - I.T (x I (n+1))‖^2 ≤ (2/(phi I (n+1))) • ⟪ (x I (n+1)) - I.T (x I (n+1)) , I.x_0 - x I (n+1)⟫) := by
   induction n
   case zero =>
     constructor
     case left =>
-      simp
       unfold phi
       simp
-    case right =>
-    have hRecFirstStep : I.T I.x_0 = (2:ℝ) • x I 1 - I.x_0 := by
-      rw [first_recurrence_term]
+      rw [base_case_recurrence]
       simp
-    have hNormSqConsecTermDiffExpanded : ‖ I.T (x I 1) ‖ ^ 2 = ‖x I 1 - I.T x I 1‖^2 + ‖ x I 1 - I.x_0 ‖^2 := by sorry
+    case right =>
+      have hRecFirstStep : I.T I.x_0 = (2:ℝ) • x I 1 - I.x_0 := by
+        rw [first_recurrence_term]
+        simp
+      have hNormSqConsecTermDiffExpanded: ‖ I.T (I.x_0) - I.T (x I 1)‖ ^ 2 = ‖x I 1 - I.T (x I 1)‖^2 + ‖x I 1 - I.x_0‖^2 + 2 * ⟪ x I 1 - I.T (x I 1), x I 1 - I.x_0 ⟫ := by
+        rw [hRecFirstStep]
+        rw [split_prod]
+        rw [comm_operation]
+        rw [norm_add_sq_real]
+        rw [add_right_comm]
+        rw [real_inner_comm]
+        simp
+        rw [add_comm]
+      have hConsecSquareNonexpansive := square_nonexpansive I I.x_0 (x I 1)
+
+      simp
+      rw [first_phi]
+      simp
+
+
+
+
   case succ n exp =>
     constructor
     case left =>
